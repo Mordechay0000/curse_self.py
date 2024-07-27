13191657e75b159f01ec983f83af40d131815f60
Mordechay <m8572998@gmail.com>
2024-07-27 22:28:08 +0300

    finish.

--- משימה מתגלגלת/hangman.py
+++ משימה מתגלגלת/hangman.py
@@ -1,4 +1,6 @@
+import os
 import random
+from enum import Enum
 
 HANGMAN_ASCII_ART = """
 Welcome to the game Hangman
@@ -69,6 +71,10 @@ def print_hangman(num_of_tries):
     print(HANGMAN_PHOTOS[str(num_of_tries)])
 
 
+def clear_screen():
+    os.system(('cls' if os.name == 'nt' else 'clear') if os.getenv('TERM') else '')
+
+
 def is_valid_input(letter_guessed):
     if (not letter_guessed.isalpha()) or len(letter_guessed) > 1:
         return False
@@ -81,11 +87,10 @@ def check_valid_input(letter_guessed, old_letters_guessed):
 
 
 def try_update_letter_guessed(letter_guessed, old_letters_guessed: list):
-    letter_guessed = letter_guessed.lower()
     if check_valid_input(letter_guessed, old_letters_guessed):
         old_letters_guessed.append(letter_guessed)
         return True
-    else:
+    elif not check_valid_input(letter_guessed, old_letters_guessed):
         print('X')
         print(' -> '.join(sorted(old_letters_guessed)))
         return False
@@ -98,7 +103,7 @@ def show_hidden_word(secret_word, old_letters_guessed):
             display_word += (letter + ' ')
         else:
             display_word += ('_ ')
-    return display_word.rstrip()
+    print(display_word.rstrip())
 
 
 def check_win(secret_word, old_letters_guessed):
@@ -106,6 +111,7 @@ def check_win(secret_word, old_letters_guessed):
         if letter not in old_letters_guessed:
             return False
     return True
+
 
 def choose_word(file_path, index):
     index -= 1
@@ -115,15 +121,51 @@ def choose_word(file_path, index):
     num = len(list(set(word_list)))
     while index >= len(word_list):
         index -= len(word_list)
-    return num, word_list[index]
-
-print(choose_word('test_file.txt', 14))
+    return word_list[index]
 
 
-"""
-try_char = input("Guess a letter: ").lower()
-word = input("Please enter a word: ")
-a = '_'
-b = ((a + ' ') * (len(word) - 1) + "_")
-print(b)
-"""
+def main():
+    while True:
+        num_of_tries = 0
+        old_letters_guessed = []
+
+        print('')
+        path = input('Enter a file path: ')
+        index = int(input('Enter a index of the word: '))
+        clear_screen()
+        secret_word = choose_word(path, index)
+
+        print(HANGMAN_ASCII_ART)
+        print('מספר נסיונות מרבי: ' + str(MAX_TRIES))
+        print('')
+
+        while not check_win(secret_word, old_letters_guessed) and num_of_tries < MAX_TRIES:
+            char = input("Guess a letter: ").lower()
+            clear_screen()
+
+            status = try_update_letter_guessed(char, old_letters_guessed)
+
+            if status and char not in secret_word:
+                num_of_tries += 1
+
+            print_hangman(num_of_tries)
+            show_hidden_word(secret_word, old_letters_guessed)
+            print('')
+
+        if check_win(secret_word, old_letters_guessed):
+            print('Nice, you won!👌')
+            print('Try again to see if you are lucky today.🔮')
+            print('')
+            if input('Would you like to try again? (y/n)').lower() == 'n':
+                exit(0)
+        if num_of_tries >= MAX_TRIES:
+            print('Sorry, you lost!')
+            print('Try again, you might get lucky.')
+            print('')
+            if input('Would you like to try again? (y/n)').lower() == 'n':
+                exit(0)
+        print('')
+
+
+if __name__ == '__main__':
+    main()
