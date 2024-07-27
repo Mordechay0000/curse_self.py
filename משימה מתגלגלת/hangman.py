c5144208a5f79f86ad25e0a523359342b8276442
Mordechay <m8572998@gmail.com>
2024-07-27 23:30:24 +0300

    Recent revisions.

--- משימה מתגלגלת/hangman.py
+++ משימה מתגלגלת/hangman.py
@@ -14,7 +14,15 @@ Welcome to the game Hangman
                      |___/
 """
 
-HANGMAN_PHOTOS = {'0': """x-------x""",
+HANGMAN_PHOTOS = {
+                  '0': """
+    x-------x
+
+
+
+
+
+""",
                   '1': """
     x-------x
     |
@@ -66,6 +74,11 @@ HANGMAN_PHOTOS = {'0': """x-------x""",
 
 MAX_TRIES = 6
 
+from enum import Enum
+
+class FileError(Enum):
+    FILE_NOT_FOUND = "File not found"
+    INVALID_FILE_FORMAT = "Invalid file format"
 
 def print_hangman(num_of_tries):
     print(HANGMAN_PHOTOS[str(num_of_tries)])
@@ -114,14 +127,30 @@ def check_win(secret_word, old_letters_guessed):
 
 
 def choose_word(file_path, index):
-    index -= 1
-    file = open(file_path, 'r')
-    word_list = file.read().split(' ')
-    file.close()
-    num = len(list(set(word_list)))
-    while index >= len(word_list):
-        index -= len(word_list)
-    return word_list[index]
+    try:
+        index -= 1
+        file = open(file_path, 'r')
+        word_list = file.read().split(' ')
+        file.close()
+        num = len(list(set(word_list)))
+        while index >= len(word_list):
+            index -= len(word_list)
+        return word_list[index]
+    except FileNotFoundError:
+        print('שגיאה: הקובץ לא נמצא')
+        return FileError.FILE_NOT_FOUND
+    except Exception:
+        print('שגיאה: פורמט הקובץ לא תקין')
+        return FileError.INVALID_FILE_FORMAT
+
+
+def is_exit():
+    while True:
+        is_next = input('Would you like to try again? (y/n)').lower()
+        if is_next == 'n':
+            exit(0)
+        elif is_next == 'y':
+            break
 
 
 def main():
@@ -129,14 +158,23 @@ def main():
         num_of_tries = 0
         old_letters_guessed = []
 
-        print('')
-        path = input('Enter a file path: ')
-        index = int(input('Enter a index of the word: '))
-        clear_screen()
-        secret_word = choose_word(path, index)
+        secret_word = ''
+        while secret_word == '' or secret_word == FileError.FILE_NOT_FOUND or secret_word == FileError.INVALID_FILE_FORMAT:
+            print('')
+            path = input('Enter a file path: ')
+            index = ''
+            while not index.isdigit():
+                index = input('Enter a index of the word: ')
+                if not index.isdigit():
+                    print('הערך שהוזן אינו מספר...')
+            index = int(index)
+            clear_screen()
+            secret_word = choose_word(path, index)
 
         print(HANGMAN_ASCII_ART)
         print('מספר נסיונות מרבי: ' + str(MAX_TRIES))
+        print('')
+        print_hangman(num_of_tries)
         print('')
 
         while not check_win(secret_word, old_letters_guessed) and num_of_tries < MAX_TRIES:
@@ -154,16 +192,16 @@ def main():
 
         if check_win(secret_word, old_letters_guessed):
             print('Nice, you won!👌')
+            print('Number of wrong guesses: ' + str(num_of_tries))
             print('Try again to see if you are lucky today.🔮')
             print('')
-            if input('Would you like to try again? (y/n)').lower() == 'n':
-                exit(0)
+            is_exit()
         if num_of_tries >= MAX_TRIES:
             print('Sorry, you lost!')
             print('Try again, you might get lucky.')
             print('')
-            if input('Would you like to try again? (y/n)').lower() == 'n':
-                exit(0)
+            is_exit()
+
         print('')
 
 
